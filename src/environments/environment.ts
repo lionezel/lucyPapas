057ac23bc75fe0721af238720bf9a186b624dd91
lionezel <lionezelfts@gmail.com>
2023-08-26 13:06:33 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'lucypapas-45283',
+    appId: '1:398872172591:web:0aae56c4057078a5ad4eda',
+    storageBucket: 'lucypapas-45283.appspot.com',
+    apiKey: 'AIzaSyAmwYVG3d5ihakLWAi_q7CJnshfDsVNkZI',
+    authDomain: 'lucypapas-45283.firebaseapp.com',
+    messagingSenderId: '398872172591',
+    measurementId: 'G-86PCS5G6KZ',
+  },
+};
