655a8e610658421e2aea344def5567436dcc6505
lionezel <lionezelfts@gmail.com>
2023-08-30 18:39:53 -0500

    componente de productbyid

---
+++ src/app/page/products/products-by-id/products-by-id.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-by-id',
+  templateUrl: './products-by-id.component.html',
+  styleUrls: ['./products-by-id.component.css']
+})
+export class ProductsByIdComponent {
+
+}
---
+++ src/app/page/products/products.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsByIdComponent } from './products-by-id/products-by-id.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductsByIdComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductsModule { }
