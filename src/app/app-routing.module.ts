5f0e17a31b705f524533a2e719a286fc548ee948
lionezel <lionezelfts@gmail.com>
2023-08-30 18:44:21 -0500

    componente de productbyid funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { HomeComponent } from './page/home/home.component';
 import { LoginComponent } from './page/user/login/login.component';
 import { RegisterComponent } from './page/user/register/register.component';
 import { AddPeoductComponent } from './page/admin/add-peoduct/add-peoduct.component';
+import { ProductsByIdComponent } from './page/products/products-by-id/products-by-id.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+
   { path: 'addProducts', component: AddPeoductComponent },
+
+  { path: 'productsById/:id', component: ProductsByIdComponent },
 ];
 
 @NgModule({
--- src/app/page/products/products.module.ts
+++ src/app/page/products/products.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ProductsByIdComponent } from './products-by-id/products-by-id.component';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ProductsByIdComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProductsByIdComponent],
+  imports: [CommonModule, SharedModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
