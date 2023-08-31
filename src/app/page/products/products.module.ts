b6c2f21b828c173ab425d8345b671c7b7b297c4d
lionezel <lionezelfts@gmail.com>
2023-08-31 11:20:44 -0500

    componente de hamburguesa

--- src/app/page/products/products.module.ts
+++ src/app/page/products/products.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HamburguerComponent } from './hamburguer/hamburguer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HamburguerComponent
+  ],
   imports: [
     CommonModule
   ]
