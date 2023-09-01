a45b490e11e81685bb15b3f634c64bed667480a7
lionezel <lionezelfts@gmail.com>
2023-09-01 08:41:36 -0500

    organizando el codigo de home

--- src/app/page/home/components/components-home.module.ts
+++ src/app/page/home/components/components-home.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CarouselModule } from './carousel/carousel.module';
+import { PromComponent } from './prom/prom.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PromComponent
+  ],
   imports: [CommonModule, CarouselModule],
+
+  exports: [
+    PromComponent
+  ]
 })
 export class ComponentsHomeModule {}
---
+++ src/app/page/home/components/prom/prom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prom',
+  templateUrl: './prom.component.html',
+  styleUrls: ['./prom.component.css']
+})
+export class PromComponent {
+
+}
