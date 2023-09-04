b2cdeebbe1cd0dd38c9bff6a8be0d9e79c1d6948
lionezel <lionezelfts@gmail.com>
2023-09-04 18:28:41 -0500

    solucion de error v2

--- src/app/page/products/products.module.ts
+++ src/app/page/products/products.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HamburguerPageComponent } from './hamburguer-page/hamburguer-page.component';
-import { ProductsByIdComponent } from './products-by-id/products-by-id.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PrimengModule } from 'src/app/primeng/primeng.module';
 import { PotatoPageComponent } from './potato-page/potato-page.component';
 
 @NgModule({
-  declarations: [ProductsByIdComponent, HamburguerPageComponent, PotatoPageComponent],
+  declarations: [ HamburguerPageComponent, PotatoPageComponent],
   imports: [CommonModule, SharedModule, PrimengModule],
 })
 export class ProductsModule {}
