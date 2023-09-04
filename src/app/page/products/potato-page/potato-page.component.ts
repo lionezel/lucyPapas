97c81881b6cd5f22f02a4565614ee94f9f1dd123
lionezel <lionezelfts@gmail.com>
2023-09-04 10:54:41 -0500

    creacion pagina de salchipapas

---
+++ src/app/page/products/potato-page/potato-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-potato-page',
+  templateUrl: './potato-page.component.html',
+  styleUrls: ['./potato-page.component.css']
+})
+export class PotatoPageComponent {
+
+}
--- src/app/page/products/products.module.ts
+++ src/app/page/products/products.module.ts
@@ -5,9 +5,10 @@ import { HamburguerPageComponent } from './hamburguer-page/hamburguer-page.compo
 import { ProductsByIdComponent } from './products-by-id/products-by-id.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PrimengModule } from 'src/app/primeng/primeng.module';
+import { PotatoPageComponent } from './potato-page/potato-page.component';
 
 @NgModule({
-  declarations: [ProductsByIdComponent, HamburguerPageComponent],
+  declarations: [ProductsByIdComponent, HamburguerPageComponent, PotatoPageComponent],
   imports: [CommonModule, SharedModule, PrimengModule],
 })
 export class ProductsModule {}
